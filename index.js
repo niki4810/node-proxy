ca6f305b8790ba2d3c90e92d71adc7a30cc51d37
Nikhilesh Katakam <NKatakam@walmart.com>
2015-04-22 23:30:06 -0700

    Step2: Echo server

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ let http = require('http')
 
 http.createServer((req, res) => {
     console.log(`Request received at: ${req.url}`)
-    res.end('hello world \n')
+    for (let header in req.headers) {
+    	res.setHeader(header, req.headers[header])
+	}
+    req.pipe(res)
 }).listen(8000)
