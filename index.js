83729df0575b62d51ff7fe4a0ef277c05cc956d9
Nikhilesh Katakam <NKatakam@walmart.com>
2015-04-22 23:36:07 -0700

    Step3: basic proxy server

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 let http = require('http')
+let request = require('request')
+let destinationUrl = '127.0.0.1:8000'
 
 http.createServer((req, res) => {
     console.log(`Request received at: ${req.url}`)
@@ -7,3 +9,15 @@ http.createServer((req, res) => {
 	}
     req.pipe(res)
 }).listen(8000)
+
+
+http.createServer((req, res) => {
+	console.log(`Proxying request to: ${destinationUrl + req.url}`)
+	let options = {
+    	headers: req.headers,
+    	url: `http://${destinationUrl}${req.url}`
+	}
+	request(options).pipe(res)
+	options.method = req.method
+	req.pipe(request(options)).pipe(res)
+}).listen(8001)
